876cb79ce55219c9a80fede17a21ebb4f48c0cdd
Reda Lemeden <git@redalemeden.com>
2020-08-23 12:19:07 +0200

    Remove Rubik

--- src/components/layouts/default.js
+++ src/components/layouts/default.js
@@ -11,7 +11,6 @@ import { ReactComponent as Pattern } from '../../images/pattern.svg';
 import '../../css/normalize.css';
 import '../../css/styles.css';
 
-import 'typeface-rubik';
 import 'typeface-inter';
 
 class DefaultLayout extends React.Component {
